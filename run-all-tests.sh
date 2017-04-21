3fd3b07c7c081f29ecce11e8ea164c7e053d186a
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-21 00:58:30 -0300

    carpeta de resultados

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,3 +1,3 @@
 for q in $(ls *.sql); do
-	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  $q.txt
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/$q.txt
 done
