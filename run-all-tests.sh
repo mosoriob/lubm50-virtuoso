ebe3bd45d627b53e420c4944f21071a43c55104b
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-21 00:57:50 -0300

    scripts para correr tests y server

---
+++ run-all-tests.sh
@@ -0,0 +1,3 @@
+for q in $(ls *.sql); do
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  $q.txt
+done
---
+++ run-virtuso.sh
@@ -0,0 +1,1 @@
+cd /opt/virtuoso-7.1.0/bin/; /opt/virtuoso-7.1.0/bin/virtuoso-t -f
