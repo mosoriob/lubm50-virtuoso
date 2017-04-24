883a282d2f2a89cb406d223f35d5bf60e40b0878
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-24 14:39:11 -0300

    Dev (#1)
    * Resultados notebook

--- fork-run-all-tests.sh
+++ fork-run-all-tests.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
+function esperar {
+	for job in `jobs -p`
+	do
+	echo $job
+	    wait $job || let "FAIL+=1"
+	done
 
+	echo $FAIL
+
+	if [ "$FAIL" == "0" ];
+	then
+	echo "YAY!"
+	else
+	echo "FAIL! ($FAIL)"
+	fi
+}
 FAIL=0
+/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
 
-for q in $(ls *.sql); do
+for i in {1..3}; do
 	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q.txt &
 done
-
-for job in `jobs -p`
-do
-echo $job
-    wait $job || let "FAIL+=1"
+esperar
+for i in {4..6}; do
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q.txt &
 done
+esperar
 
-echo $FAIL
-
-if [ "$FAIL" == "0" ];
-then
-echo "YAY!"
-else
-echo "FAIL! ($FAIL)"
-fi
 #clean
 /etc/init.d/virtuoso restart
--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,5 +1,6 @@
-for q in $(ls *.sql); do
-	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  $results/s/$q.txt
+/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
+for q in $(ls q*.sql); do
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/s/$q.txt
 done
 #clean
 /etc/init.d/virtuoso restart
