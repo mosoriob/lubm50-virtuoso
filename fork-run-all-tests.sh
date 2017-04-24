bf0ebcfdb4f42f4caf2865a06382b1ce505a85bf
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-23 23:15:57 -0300

    pre-clean

--- fork-run-all-tests.sh
+++ fork-run-all-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 FAIL=0
-
-for q in $(ls *.sql); do
+/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lumb-load.sql
+for q in $(ls q*.sql); do
 	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q.txt &
 done
 
--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,4 +1,5 @@
-for q in $(ls *.sql); do
+/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lumb-load.sql
+for q in $(ls q*.sql); do
 	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/s/$q.txt
 done
 #clean
