240439d62481503cbef2e5da340be1f9cce28dab
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-26 20:25:31 -0300

    status: scripts para ver el numero de triples

--- fork-run-all-tests.sh
+++ fork-run-all-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FAIL=0
 q=$1
-i=$2
+iter=$2
 
 
 /opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
 echo "Realizando test: query $q con $i iteraciones"
-for i in {1..$2}; do
+for i in seq($iter); do
 	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q-$i.txt &
 done
 
