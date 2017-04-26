3f51a956e14aa643a29912789f7f0a63147879f9
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-26 20:43:17 -0300

    querys

--- fork-run-all-tests.sh
+++ fork-run-all-tests.sh
@@ -4,10 +4,10 @@ q=$1
 iter=$2
 
 
-/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
+#/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
 echo "Realizando test: query $q con $i iteraciones"
 for i in $(seq $iter); do
-	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q-$iter.txt &
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec | cut -d ' ' -f 4 >>  results/p/$q-$iter.txt &
 done
 
 for job in `jobs -p`
