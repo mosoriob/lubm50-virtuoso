2e057fd6e9692005a2ebdada43e868f219b359c2
Maximiliano Osorio <maxiosorio@gmail.com>
2017-04-26 19:30:16 -0300

    se añaden la posibilidad de iteraciones

--- fork-run-all-tests.sh
+++ fork-run-all-tests.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
-function esperar {
-	for job in `jobs -p`
-	do
-	echo $job
-	    wait $job || let "FAIL+=1"
-	done
-
-	echo $FAIL
-
-	if [ "$FAIL" == "0" ];
-	then
-	echo "YAY!"
-	else
-	echo "FAIL! ($FAIL)"
-	fi
-}
 FAIL=0
+q=$1
+i=$2
+
 /opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < lubm-load.sql
-
-for i in {1..3}; do
-	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q.txt &
+echo "Realizando test: query $q con $i iteraciones"
+for i in {1..$2}; do
+	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q-$1.txt &
 done
-esperar
-for i in {4..6}; do
-	/opt/virtuoso-7.1.0/bin/isql-vt 1111 dba b4LyNAYmIfflH84JS1P5 < $q  | grep msec >>  results/p/$q.txt &
-done
-esperar
 
-#clean
-/etc/init.d/virtuoso restart
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+echo "YAY!"
+else
+echo "FAIL! ($FAIL)"
+fi
